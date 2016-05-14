4ba9b41eaed3195bf1684a832fdcf10cd101ddb7
Paulo Menezes <paulo.hgmenezes@gmail.com>
2016-05-13 23:37:25 -0300

    MongoDB

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'secret': 'medhelp.projetao.2016.1',
-	'database': 'mongodb://localhost/medhelp'
+	'database': 'mongodb://paulo:paulo@ds023042.mlab.com:23042/heroku_911mqx4r'
 }
