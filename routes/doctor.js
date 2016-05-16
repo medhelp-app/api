43fd1a3e9b2f440cb9c7e14d87e391f33c9900f7
Paulo Menezes <paulo.hgmenezes@gmail.com>
2016-05-16 19:50:49 -0300

    Find doctors

--- routes/doctor.js
+++ routes/doctor.js
@@ -5,36 +5,60 @@ var DoctorController = require('../controllers/doctor');
 var doctorController = new DoctorController();
 
 router.route('/').get(function (req, res) {
-    doctorController.getAll(function (doctors, error) {
-        if (error) {
-            res.status(404);
-            res.send(error);
-        } else {
-            res.json(doctors);
-        }
-    });
+	doctorController.getAll(function (doctors, error) {
+		if (error) {
+			res.status(404);
+			res.send(error);
+		} else {
+			res.json(doctors);
+		}
+	});
 });
+
+router.route('/find').get(function (req, res) {
+	doctorController.getAll(function (doctors, error) {
+		if (error) {
+			res.status(404).send(error);
+		} else {
+			var address = [];
+			for (var i = 0; i < doctors.length; i++) {
+				address.push({
+					id: doctors[i].id,
+					addressStreet: doctors[i].addressStreet,
+				    addressNumber: doctors[i].addressNumber,
+				    city: doctors[i].city,
+				    state: doctors[i].state,
+				    zipCode: doctors[i].zipCode,
+				    country: doctors[i].country,
+				});
+			};
+
+			res.json(address);
+		}
+	});
+});
+
 router.route('/:id').get(function (req, res) {
-    doctorController.getForId(req.params.id,function (userFull, error) {
-        if(error){
-            res.status(404);
-            res.send(error);
-        }else{
-            res.status(200);
-            res.json(userFull);
-        }
-    })
+	doctorController.getForId(req.params.id,function (userFull, error) {
+		if(error){
+			res.status(404);
+			res.send(error);
+		}else{
+			res.status(200);
+			res.json(userFull);
+		}
+	})
 })
 
 router.route('/:id').put(function (req, res) {
-    doctorController.update(req.params.id, req.body, function (doctor, error) {
-        if (error) {
-            res.status(400);
-            res.send(error);
-        } else {
-            res.json(doctor);
-        }
+	doctorController.update(req.params.id, req.body, function (doctor, error) {
+		if (error) {
+			res.status(400);
+			res.send(error);
+		} else {
+			res.json(doctor);
+		}
 
-    });
+	});
 });
 module.exports = router;
