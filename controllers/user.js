322d23eb638aaa99d5eefb246a3aa696217ac80b
jeremias leite <jeremias leite>
2016-06-14 21:04:24 -0300

    fix secretary

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,7 @@ var Doctor = require('../models/doctor');
 var sha512 = require('sha512');
 var fs = require('fs');
 var nodemailer = require('nodemailer');
+var Secretary = require('../models/secretary');
 
 var token = require('token');
 token.defaults.secret = 'medhelp';
@@ -95,7 +96,7 @@ UserController.prototype.insert = function(_user, callback) {
 			if (functions.validateEmail(_user.email)) {
 				if (_user.password == _user.rePassword && _user.password.length >= 6) {
 						if (users.length === 0) {
-							if(_user.userType == 0 || _user.userType == 1 ||_user.userType == 2){
+							if(_user.userType == 0 || _user.userType == 1){
 
 								if (_user.name) {
 									var user = new User();
@@ -141,7 +142,7 @@ UserController.prototype.insert = function(_user, callback) {
 														});	
 													}
 												});
-											} else if (userType == 1){
+											} else{
 												var doctor = new Doctor();
 												doctor._id = _user._id;
 												doctor.addressStreet = "";
@@ -168,8 +169,6 @@ UserController.prototype.insert = function(_user, callback) {
 														});
 													}
 												});
-											}else{
-												callback(_user);
 											}
 										}
 									});
@@ -332,7 +331,7 @@ UserController.prototype.updateImage = function (id, _image, callback) {
         }     
     });             
 };
-/*----Secretary---*/
+/*---------Secretary-----------*/
 UserController.prototype.listSecretary = function(callback) {
 	console.log('entrou')
 	User.find({userType: '2'},function (error, users) {
@@ -349,6 +348,115 @@ UserController.prototype.findNameSecretary = function(name, callback) {
 			callback(null, error);
 		} else {
 			callback(users);
+		}
+	});
+};
+UserController.prototype.getIdSecretary = function (idUser, callback) {
+	User.findById(idUser,function (error, user) {
+		if(error){
+			callback(null, error);
+		}else{
+			if(user){
+				Secretary.findById(idUser,function (error, secretary) {
+					if(error){
+						callback({Error: 'Error ao buscar!'})
+					}else{
+						callback({user:user,secretary:secretary });
+
+					}
+				})
+			}
+
+		}
+	})
+};
+UserController.prototype.insertSecretary = function(_user, callback) {
+	var functions = this.functions;
+
+	User.findById(_user.doctorId, function (error, doctor) {
+		if(error){
+			callback({Error: 'Error insperado!'})
+		}else {
+			if(doctor){
+				User.find({ email: _user.email }, function (error, users) {
+					if (error) {
+						callback(null, error);
+					} else {
+						if (functions.validateEmail(_user.email)) {
+							if (_user.password == _user.rePassword && _user.password.length >= 6) {
+								if (users.length === 0) {
+									if (_user.name) {
+										var user = new User();
+										user.userType = '2';
+										user.name = _user.name;
+										user.email = _user.email;
+										user.password = sha512(_user.password).toString('hex');
+										user.save(function (error, _user) {
+											var mailOptions = {
+												from: '"MedHelp Time" <medhelp.noreply@gmail.com>',
+												to: _user.email,
+												subject: 'Bem Vindo ao Medhelp',
+												text: 'Bem Vindo ao Medhelp',
+												html: '<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml"><head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <meta name="viewport" content="width=device-width" /> <!-- For development, pass document through inliner --> </head><body style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;width: 100% !important;height: 100%;background: #efefef;-webkit-font-smoothing: antialiased;-webkit-text-size-adjust: none"><table class="body-wrap" style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;width: 100% !important;height: 100%;background: #efefef;-webkit-font-smoothing: antialiased;-webkit-text-size-adjust: none"> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td class="container" style="margin: 0 auto !important;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;display: block !important;clear: both !important;max-width: 580px !important"> <!-- Message start --> <table style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;width: 100% !important;border-collapse: collapse"> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td align="center" class="masthead" style="margin: 0;padding: 80px 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;background: #336699;color: white"> <h1 style="margin: 0 auto !important;padding: 0;font-size: 32px;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.25;margin-bottom: 20px;max-width: 90%;text-transform: uppercase">Bem vindo ao Medhelp</h1> </td> </tr> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td class="content" style="margin: 0;padding: 30px 35px;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;background: white"> <h2 style="margin: 0;padding: 0;font-size: 28px;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.25;margin-bottom: 20px">Olá, você agora faz parte do MedHelp</h2> <p style="margin: 0;padding: 0;font-size: 16px;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;font-weight: normal;margin-bottom: 20px">MedHelp é uma solução para facilitar na comunicação entre os pacientes e médicos.</p> <table style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;width: 100% !important;border-collapse: collapse"> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td align="center" style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <p style="margin: 0;padding: 0;font-size: 16px;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;font-weight: normal;margin-bottom: 20px"> <a href="http://medhelp-app.github.io/" class="button" style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;color: white;text-decoration: none;display: inline-block;background: #336699;border: solid #336699;border-width: 10px 20px 8px;font-weight: bold;border-radius: 4px">Acesse sua conta</a> </p> </td> </tr> </table> </td> </tr> </table> </td> </tr> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td class="container" style="margin: 0 auto !important;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;display: block !important;clear: both !important;max-width: 580px !important"> <!-- Message start --> <table style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;width: 100% !important;border-collapse: collapse"> <tr style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65"> <td class="content footer" align="center" style="margin: 0;padding: 30px 35px;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;background: none"> <p style="margin: 0;padding: 0;font-size: 14px;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;font-weight: normal;margin-bottom: 0;color: #888;text-align: center">Enviado por <a href="http://medhelp-app.github.io/" style="margin: 0;padding: 0;font-size: 100%;font-family: &quot;Avenir Next&quot;, &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;line-height: 1.65;color: #888;text-decoration: none;font-weight: bold">MedHelp</a></p> </td> </tr> </table> </td> </tr></table></body></html>'
+											};
+											if (error) {
+												callback(null, error);
+											} else {
+												var secretary = new Secretary();
+												secretary.doctorId = doctor._id;
+												secretary._id = _user._id;
+												secretary.save(function (error, resul) {
+													if (error) {
+														callback(null, error);
+													} else {
+														transporter.sendMail(mailOptions, function(error, info){
+															if(error){
+																callback(null,error);
+															}
+															else{
+																callback(_user);
+															}
+														});
+													}
+												});
+
+											}
+										});
+									} else {
+										callback(null, { error: 'O campo \'name\' é obrigatório.' });
+									}
+
+								} else {
+									callback(null, { error: 'E-mail duplicado.' });
+								}
+							} else {
+								callback(null, { error: 'Senhas inválidas.' })
+							}
+						} else {
+							callback(null, { error: 'E-mail inválido.' })
+						}
+					}
+				});
+			}else{
+				callback({Error: 'Doctor não existe!'})
+			}
+		}
+	})
+
+};
+UserController.prototype.deleteSecretary = function(id, callback) {
+	User.remove({ _id: id }, function (error) {
+		if (error) {
+			callback(null, { error: 'ID inválido.' });
+		} else {
+			Secretary.remove({ _id: id }, function (error) {
+				if (error) {
+					callback(null, {error: 'ID inválido.'});
+				}else{
+					callback({ message: 'Removido com sucesso.' });
+				}
+			})
+
 		}
 	});
 };
